9cd99f6c74111589caae2beacb575b5e2291aed0
mslalor <mslalor@crimson.ua.edu>
2024-02-13 15:54:40 -0600

    Fixed PWEB, login button, potential js file

---
+++ websiteDesignIdeas/accounts.js
@@ -0,0 +1,33 @@
+class Account{
+    constructor (username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.numBooks = 0;
+        this.bookList = [];
+    }
+
+    addBook(book){
+        this.bookList[numBooks] = book;
+        numBooks++;
+    }
+
+    checkOut(book){
+        print("finish this function");
+    }
+
+
+}
+
+class Book{
+    constructor(title, author, isbn, status){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.status = status;
+    }
+
+    editStatus(){
+        this.status = !this.status;
+    }
+}
