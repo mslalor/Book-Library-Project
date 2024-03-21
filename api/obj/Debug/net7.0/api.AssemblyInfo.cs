4bbe1b5bcec24e7440893fe589b1fc3907fae3cc
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-03-21 15:06:00 -0500

    ran

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ffac1eb72fa89b0ef6a7fcdabd2ba4f6c2a1e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0576458a5f85ec6f43c012a063695d56f01e54c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
