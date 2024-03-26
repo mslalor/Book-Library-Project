ccf93f7afb47c732268fe218af3ea0bb678c4605
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-03-26 15:56:10 -0500

    fixed name for users
    Name for users in dashboard

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8250a9e2ce354e01034ebe1316b45bb5dd49cc68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddef68fdcf5e63172248b0e54064d3b73a6735d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- websiteDesignIdeas/accounts.js
+++ websiteDesignIdeas/accounts.js
@@ -86,26 +86,26 @@ function getID(){
     return 1; 
 }
 
-function getBooks(user){
-    //replace the below lines with queries to the user's book list. If empty then just return null.
-    //(it should only be empty most likely if there is a new account)
-    //for now this is just our tester. 
-    /* I was thinking something like this. 
-    for(var i = 0; i<user.numBooks){
-        var title = query for title of book i 
-        var author = ^
-        var isbn = ^
-        var status = ^
-        var img = ^
-        let book = new Book (title, author, isbn, status, img);
-        user.addBook(book);
-    }
+// function getBooks(user){
+//     //replace the below lines with queries to the user's book list. If empty then just return null.
+//     //(it should only be empty most likely if there is a new account)
+//     //for now this is just our tester. 
+//     /* I was thinking something like this. 
+//     for(var i = 0; i<user.numBooks){
+//         var title = query for title of book i 
+//         var author = ^
+//         var isbn = ^
+//         var status = ^
+//         var img = ^
+//         let book = new Book (title, author, isbn, status, img);
+//         user.addBook(book);
+//     }
 
-    */
-    let book1 = new Book("Harry Potter", "JK Rowling", "0000", "In Library", "img/potter7.jpeg")
-    user.addBook(book1);
-    let book2 = new Book("Hunger Games", "Suzanne Collins", "0001", "In Library", "img/games1.jpeg");
-    user.addBook(book2);
-    let book3 = new Book("I am Malala", "Malala", "0002", "In Library", "img/malala.jpeg");
-    user.addBook(book3);    
-}
+//     */
+//     let book1 = new Book("Harry Potter", "JK Rowling", "0000", "In Library", "img/potter7.jpeg")
+//     user.addBook(book1);
+//     let book2 = new Book("Hunger Games", "Suzanne Collins", "0001", "In Library", "img/games1.jpeg");
+//     user.addBook(book2);
+//     let book3 = new Book("I am Malala", "Malala", "0002", "In Library", "img/malala.jpeg");
+//     user.addBook(book3);    
+// }
