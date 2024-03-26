737af1e2059cb5dc1a59b5d0a3fff316e0c27005
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-03-26 11:03:42 -0500

    ran the code
    code ran

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bbd3e62cef36bf3d4c7ff2911713596b50822b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8250a9e2ce354e01034ebe1316b45bb5dd49cc68")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
