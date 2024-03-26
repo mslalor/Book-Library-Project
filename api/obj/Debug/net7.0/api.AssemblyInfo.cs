3b998f579c0d64a2d3ac9ddec0edba15b92a851c
mtkantorski <95382964+mtkantorski@users.noreply.github.com>
2024-03-26 00:41:32 -0500

    Added Welcome and Bio page

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9954cf3655590ec7a5e0ebd7b620393ed6eee048")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bbd3e62cef36bf3d4c7ff2911713596b50822b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
