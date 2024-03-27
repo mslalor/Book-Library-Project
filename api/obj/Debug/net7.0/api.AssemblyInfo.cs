a173eca3f21354470f1f0a95370387954a62e0f4
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-03-27 17:11:12 -0500

    rent
    tried to edit rent

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb5a6b8ffe355682181bd64b37e599fff10a913")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05ba07fec01f8120d7f20efa613b81ce72516b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
