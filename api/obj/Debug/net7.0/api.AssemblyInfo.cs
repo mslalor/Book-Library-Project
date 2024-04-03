bfd49a41f6d800cd82b37444e50eefbcf682a394
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-04-02 21:55:02 -0500

    added netlify file
    added the file for netlify

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+537f881b0b7079553584127eee7c33fd7e7654ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85448dc213b61cecae507b3a8d13ab7718cae713")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
