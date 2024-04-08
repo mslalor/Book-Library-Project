a809761f893bc70735b8f8905a654d270dcd57cd
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-04-08 16:06:39 -0500

    change url
    DEPLOYED

--- obj/Debug/net8.0/dotnetFolder.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnetFolder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetFolder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127e179c3f4ab13a910feaef55eff803a1229a28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6503ce4b8e96c94b2af15de394e04c7ae88319c")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetFolder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetFolder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
