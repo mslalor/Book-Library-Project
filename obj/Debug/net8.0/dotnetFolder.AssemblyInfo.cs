49d552fcf045648ef8785e7f6ed28c7cf9548e3f
mtkantorski <95382964+mtkantorski@users.noreply.github.com>
2024-04-22 21:56:08 -0500

    filter password for invalid characters/symbols

--- obj/Debug/net8.0/dotnetFolder.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnetFolder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetFolder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184914ab357363ecd9a5963e3b6ad965ece3c20a")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetFolder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetFolder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
