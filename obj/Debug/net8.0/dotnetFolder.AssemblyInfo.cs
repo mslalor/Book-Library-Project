4106268419c49af9b3d3db82cef0a2f6d3129aa5
klandrzejewski <klandrzejewski@crimson.ua.edu>
2024-04-24 11:41:17 -0500

    updated email.js

--- obj/Debug/net8.0/dotnetFolder.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnetFolder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnetFolder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb0a7b892818862f626ee645906e8098ecc83c9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e4d0e7f9ce2b4b0176dc43efce3a99e5d26fb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnetFolder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnetFolder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
